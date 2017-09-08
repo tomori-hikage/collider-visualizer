5cc91f19dcd6815a0dd26887c55e5769106b9a93
mori_akito <tomori.aki2961@gmail.com>
2017-09-08 11:39:01 +0900

    使用例の実装

---
+++ collider-visualizer/Assets/Scripts/Example.cs
@@ -0,0 +1,38 @@
+﻿using HC.Debug;
+using UnityEngine;
+
+
+namespace HC
+{
+    /// <summary>
+    /// 使用例
+    /// </summary>
+    [DisallowMultipleComponent]
+    public class Example : MonoBehaviour
+    {
+        #region フィールド / プロパティ
+
+        [SerializeField]
+        private ColliderVisualizer _cubeColliderVisualizer;
+
+        [SerializeField]
+        private ColliderVisualizer _sphereColliderVisualizer;
+
+        [SerializeField]
+        private ColliderVisualizer _capsuleColliderVisualizer;
+
+        #endregion
+
+
+        #region イベントメソッド
+
+        private void Start()
+        {
+            _cubeColliderVisualizer.CreateLabel("Cube", 24);
+            _sphereColliderVisualizer.CreateLabel("Sphere", 24);
+            _capsuleColliderVisualizer.CreateLabel("Capsule", 24);
+        }
+
+        #endregion
+    }
+}
